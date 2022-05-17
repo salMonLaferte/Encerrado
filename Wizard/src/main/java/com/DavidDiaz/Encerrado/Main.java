c2af066ef13e59539a7807ce05b76f31a3d1a526
salMonLaferte <david_diaz@ciencias.unam.mx>
2022-05-17 01:19:25 -0500

    Initial commit

---
+++ Wizard/src/main/java/com/DavidDiaz/Encerrado/Main.java
@@ -0,0 +1,10 @@
+package com.DavidDiaz.Encerrado;
+
+/**
+ *Clase main que no extiende Aplicacion para que javafx funcione. 
+ */
+public class Main {
+    public static void main(String[] args ){
+        App.main(args);
+    }
+}
