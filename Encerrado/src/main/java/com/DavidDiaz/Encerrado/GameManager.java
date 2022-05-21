832f493a5ced0c6c62207c8e32446d2725927e1a
José David García Díaz <david_diaz@ciencias.unam.mx>
2022-05-20 20:42:00 -0500

    Random plays from IA and animation

--- Encerrado/src/main/java/com/DavidDiaz/Encerrado/GameManager.java
+++ Encerrado/src/main/java/com/DavidDiaz/Encerrado/GameManager.java
@@ -1,11 +1,19 @@
 package com.DavidDiaz.Encerrado;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import com.DavidDiaz.Encerrado.Board.Player;
+
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 
 public class GameManager {
 
     public static Board board = new Board(Player.Blue);
-
+    public static boolean playingAgainstIA = true;
+    public static Player iaPlayer = Player.Red;
+ 
 
     public static void mainLoop(){
         /*while(true){
@@ -34,18 +42,67 @@ public class GameManager {
     }
 
     /**
-     * Trys to make a move from the position from to the empty position, if is a valid move updates the visuals
+     * Trys to make a move from the position from to the empty position, if is a valid move updates the visuals and checks for winner
      * @param from
      */
     public static void makeMove(int from){
+        if(playingAgainstIA && board.currentTurn == iaPlayer)
+            return;
         if(!board.makeMove(from)){
             return;
         }
+        updateVisualsAndCheckForWinner();
+        if(playingAgainstIA){
+            PauseTransition p = new PauseTransition(Duration.millis(1000));
+            p.play();
+            p.setOnFinished(e->{
+                makeIAMove();
+            });
+        }
+    }
+
+    /**
+     * Makes the ia move
+     */
+    public static void makeIAMove(){
+        int move = getIARAndomMove();
+        board.makeMove(move);
+        updateVisualsAndCheckForWinner();
+    }
+
+    /**
+     * Updates visuals of the board and checks if game is over
+     */
+    public static void updateVisualsAndCheckForWinner(){
         App.updateTokens();
         App.updateTurnIndicator();
         Player winner = board.checkGAmeOver();
         if(winner != Player.None){
             App.showMessageToUser("Se termino el juego", "Ganó: " + winner);
+        }
+    }
+
+    /**
+     * Gets a Random move for the IA player.
+     * @return
+     */
+    public static int getIARAndomMove(){
+        int[] moves = new int[2];
+        moves[0] = -1;
+        moves[1] = -1;
+        int current = 0;
+        for(int i=0; i<5; i++){
+            if(board.positions[i] == iaPlayer && board.isValid(i)){
+                moves[current] = i;
+                current++;
+            }
+        }
+        if(moves[0] != -1 && moves[1] != -1){
+            Random random = new Random();
+            return moves[random.nextInt(2)];
+        }
+        else{
+            return moves[0];
         }
     }
 
